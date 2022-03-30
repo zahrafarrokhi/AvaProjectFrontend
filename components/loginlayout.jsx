1c7380be53047eb95a1b58d577bf5d7e6a09518d
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-03-30 16:15:36 +0430

    create layout and install scss

---
+++ components/loginlayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../styles/loginlayout.module.scss'
+
+const  loginlayout = (props) =>{
+  return (
+    <div>loginlayout</div>
+  )
+}
+export default loginlayout;
