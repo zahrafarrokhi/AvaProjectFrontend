2526cd4fad567b00a507aea1c1e80292ad52487d
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-03-30 17:41:35 +0430

    fix mobile styles

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -2,24 +2,38 @@ import { Button } from "@mui/material";
 import React from "react";
 import LoginLayout from "../../components/loginlayout";
 import styles from "../../styles/login.module.scss";
-function login() {
+import { useTheme } from "@mui/material/styles";
+
+function Login() {
+  const theme = useTheme();
   return (
     <div className={`d-flex flex-column`}>
       <div className="d-flex flex-column my-3">
-        <label className={`d-flex m-1 ${styles.subtitle}`}>ورود / ثبت‌‌نام</label>
+        <label className={`d-flex m-1 ${styles.subtitle}`}>
+          ورود / ثبت‌‌نام
+        </label>
         <input
           type="text"
           placeholder="شماره موبایل"
           className={`form-control ${styles.phone_input}`}
         />
       </div>
-      <Button variant="contained" sx={{ width: "300px",padding:'0.5em' }}>
+      <Button
+        variant="contained"
+        sx={{
+          width: "300px",
+          padding: "0.5em",
+          [theme.breakpoints.down("md")]: {
+            width: "240px",
+          },
+        }}
+      >
         ورود
       </Button>
     </div>
   );
 }
-login.getLayout = (page) => {
+Login.getLayout = (page) => {
   return <LoginLayout>{page}</LoginLayout>;
 };
-export default login;
+export default Login;
