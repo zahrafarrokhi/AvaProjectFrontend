a0b7dda35a3acf842a1b706dd69f3ef6ee5a1319
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-03-30 16:49:57 +0430

    create login page

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,11 +1,17 @@
 import { Button } from '@mui/material';
 import React from 'react'
 import loginlayout from '../../components/loginlayout';
-
+import styles from '../../styles/login.module.scss'
  function login() {
   return (
     <div className={`d-flex flex-column`}>
-      
+      <div className="d-flex flex-row">
+        <label>ورود / ثبت‌‌نام</label>
+        <input type="text"
+           placeholder='شماره موبایل'
+           className={`form-control ${styles.phone_input}`}
+        />
+      </div>
       <Button variant="contained" sx={{}}>ورود</Button>
     </div>
   )
