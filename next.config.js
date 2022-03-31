78ca4a0c3afd69fb75b357b5fe123942af38b722
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-03-31 14:46:11 +0430

    config svg

--- next.config.js
+++ next.config.js
@@ -19,6 +19,42 @@ const nextConfig = {
     //   },
     // ],
   },
+
+  esModule: true,
+  exclude: /\.svg$/,
+  poweredByHeader: false,
+  inlineImageLimit: false,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.(js|ts)x?$/],
+        // test: /\.(js|ts)x?$/,
+      },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: { removeViewBox: false },
+                  },
+                },
+              ],
+            },
+            titleProp: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -3,11 +3,17 @@ import React from "react";
 import LoginLayout from "../../components/loginlayout";
 import styles from "../../styles/login.module.scss";
 import { useTheme } from "@mui/material/styles";
+import MainIcon from "../../public/icons/tooth-svgrepo-com.svg";
 
 function Login() {
   const theme = useTheme();
   return (
-    <div className={`d-flex flex-column`}>
+    <div
+      className={`d-flex flex-column flex-grow-1 align-self-stretch align-items-center justify-content-center`}
+    >
+      <MainIcon
+        className={`d-flex d-md-none align-self-center ${styles.icontooth}`}
+      />
       <div className="d-flex flex-column my-3">
         <label className={`d-flex m-1 ${styles.subtitle}`}>
           ورود / ثبت‌‌نام
