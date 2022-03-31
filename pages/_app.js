caeb8bff72884476a233ac1035e2ccf1af2b41a3
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-03-31 13:18:30 +0430

    Add theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
 
-// https://nextjs.org/docs/basic-features/layouts
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// https://mui.com/customization/theming/
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3170ea",
+    },
+    secondary: {
+      main: "#eaa338",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          boxShadow: "0px 4px 20px rgba(31, 86, 192, 0.3)",
+        },
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page)
+  // https://nextjs.org/docs/basic-features/layouts
+  const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <ThemeProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
